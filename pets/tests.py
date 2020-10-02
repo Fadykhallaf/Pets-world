41142b593c95856b4b08eceb53901adf7398fdc8
fady khallaf <fady.khallaf@gmail.com>
2020-10-02 23:34:02 +0200

    add functional test

--- pets/tests.py
+++ pets/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from selenium import webdriver
 
-# Create your tests here.
+
+class FunctionalTestCase(TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def test_is_there_homepage(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('Pepe', self.browser.page_source)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
